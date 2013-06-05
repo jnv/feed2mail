13fe45d53f8c05b05a2087d99451b77ede4ed01d
Jan Vlnas <git@jan.vlnas.cz>
2013-06-05 16:48:15 +0200

    Config example

--- config.php
+++ config.php
@@ -1,3 +1,7 @@
 <?php
-define('FEED_URL', '');
-define('FILTER_KW', 'otr');
+define('FEED_URL', 'http://feeds.feedburner.com/ICanHasCheezburger');
+// define('FILTER_KW', '');
+// define('INTERVAL', 30*60);
+define('EMAIL_TO', 'rcpt@example.com');
+define('EMAIL_FROM', 'sender@example.com');
+define('SUBJECT_PREFIX', '[Feed]');
