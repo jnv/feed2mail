89f1d3ab5c7f1212f89ab552cd6be91a0b6ccbdd
Jan Vlnas <git@jan.vlnas.cz>
2013-06-10 22:44:07 +0200

    New cfg variable: STORE_PATH

--- config.php
+++ config.php
@@ -5,3 +5,4 @@ define('FEED_URL', 'http://feeds.feedburner.com/ICanHasCheezburger');
 define('EMAIL_TO', 'rcpt@example.com');
 define('EMAIL_FROM', 'sender@example.com');
 define('SUBJECT_PREFIX', '[Feed]');
+define('STORE_PATH', 'cache');
